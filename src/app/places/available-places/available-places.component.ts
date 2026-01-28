bfc0af45304f478fd88992fcd6f6e2ed9a56048d
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-29 00:22:09 +0530

    Error handling

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -13,30 +13,43 @@ import { map } from 'rxjs';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent]
 })
-export class AvailablePlacesComponent implements OnInit{
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   // private httpClient = inject(HttpClient); way 1
   private destroyRef = inject(DestroyRef);
   public isFetching = signal<boolean>(false);
-  constructor(private httpClient: HttpClient){
+  public error = signal<string>('');
+  constructor(private httpClient: HttpClient) {
 
   }
   ngOnInit(): void {
     this.isFetching.set(true);
-    const subs = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').
-    pipe(map((val) => val.places)).subscribe({
-      next: (resData)=> {
-        this.places.set(resData);
-        // console.log(response.body?.places);
-        // console.log(event);
-        // console.log(resData.places);
-      },
-      complete: () => {
-        this.isFetching.set(false);
-      }
-    });
+    const subs = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').
+      pipe(map((val) => val.places),
+        catchError((err, observable) => {
+          console.log(err);
+          return throwError(() => {
+            return new Error('Something went wrong in fetching')
+          });
+        }))
+      .subscribe({
+        next: (resData) => {
+          this.places.set(resData);
+          // console.log(response.body?.places);
+          // console.log(event);
+          // console.log(resData.places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+        error: (err: Error) => {
+          console.log(err);
+          this.error.set(err.message);
+          // this.error.set("Something went wrong fetching available places. Try again later!");
+        }
+      });
 
-    this.destroyRef.onDestroy(()=> {
+    this.destroyRef.onDestroy(() => {
       subs.unsubscribe();
     })
   }
