17f3b2ae4148c42d12d140854856f2adef905e9b
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-28 22:51:23 +0530

    config object observe response

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -12,11 +12,26 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent]
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit{
   places = signal<Place[] | undefined>(undefined);
   // private httpClient = inject(HttpClient); way 1
-  
+  private destroyRef = inject(DestroyRef);
   constructor(private httpClient: HttpClient){
 
+  }
+  ngOnInit(): void {
+    const subs = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places',
+      {observe: 'response'}                                     //config object
+    ).subscribe({
+      next: (response)=> {
+        console.log(response.body?.places);
+        console.log(response);
+        // console.log(resData.places);
+      }
+    });
+
+    this.destroyRef.onDestroy(()=> {
+      subs.unsubscribe();
+    })
   }
 }
