77058c8a4284d4a94d363875d516feeef5b82b82
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-29 00:00:26 +0530

    Fetching & populating data and fallback text

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -16,17 +17,22 @@ export class AvailablePlacesComponent implements OnInit{
   places = signal<Place[] | undefined>(undefined);
   // private httpClient = inject(HttpClient); way 1
   private destroyRef = inject(DestroyRef);
+  public isFetching = signal<boolean>(false);
   constructor(private httpClient: HttpClient){
 
   }
   ngOnInit(): void {
-    const subs = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places',
-      {observe: 'events'}                                     //config object
-    ).subscribe({
-      next: (event)=> {
+    this.isFetching.set(true);
+    const subs = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').
+    pipe(map((val) => val.places)).subscribe({
+      next: (resData)=> {
+        this.places.set(resData);
         // console.log(response.body?.places);
-        console.log(event);
+        // console.log(event);
         // console.log(resData.places);
+      },
+      complete: () => {
+        this.isFetching.set(false);
       }
     });
 
