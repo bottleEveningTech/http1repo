1f360f8723381a994c698ecc583cf7374192170b
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-02-01 19:54:03 +0530

    tap request/response in interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()]
+    providers: [provideHttpClient(
+        withInterceptors([loggingInterceptor])
+    )]
 }).catch((err) => console.error(err));
+
+
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn){
+    const req = request.clone({
+        headers: request.headers.set('X-DEBUG', 'TEST')
+    });
+    console.log('Outgoing request');
+    console.log(request);
+    // return next(req);
+    return next(request).pipe(
+        tap({
+            next: (event:any) => {
+                if(event.type === HttpEventType.Response){
+                    console.log('incoming respone');
+                    console.log(event.status);
+                }
+            }
+        })
+    );
+}
